4fd5404caebe955a0237b74325172cda29118c86
stein820 <90308864+stein820@users.noreply.github.com>
2022-06-17 01:07:14 +0700

    use linq

---
+++ DB layer/DB.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+using System.Data;
+
+namespace TungMovie
+{
+    class DB
+    {
+
+        SqlConnection con = new SqlConnection(@"Data Source=LAPTOP-3NHLU4HH\DUCNGUYEN;Initial Catalog=Movie_ticket_management;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+
+        public SqlConnection GetConnection
+        {
+            get
+            {
+                return con;
+            }
+        }
+
+        public void openConnection()
+        {
+            if ((con.State == System.Data.ConnectionState.Closed))
+            {
+                con.Open();
+            }
+        }
+
+        public void closeConnection()
+        {
+            if ((con.State == System.Data.ConnectionState.Open))
+            {
+                con.Close();
+            }
+        }
+    }
+}
